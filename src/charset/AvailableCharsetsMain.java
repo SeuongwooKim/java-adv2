2b97e473b786620351d954248896c6ddb704efda
KimSeoungWoo <suong22@naver.com>
2026-08-24 11:25:50 +0900

    java 파일 입출력 공부

--- src/charset/AvailableCharsetsMain.java
+++ src/charset/AvailableCharsetsMain.java
@@ -5,16 +5,16 @@ import java.nio.charset.StandardCharsets;
 import java.util.Set;
 import java.util.SortedMap;
 
-public class AvailableCharsetMain {
+public class AvailableCharsetsMain {
 
     public static void main(String[] args) {
-        // 이용가능한 모든 Charset 자바 + OS
+        // 이용 가능한 모든 Charset 자바 + OS
         SortedMap<String, Charset> charsets = Charset.availableCharsets();
         for (String charsetName : charsets.keySet()) {
             System.out.println("charsetName = " + charsetName);
         }
 
-        System.out.println("====");
+        System.out.println("=====");
         // 문자로 조회(대소문자 구분X), MS949, ms949, x-windows-949
         Charset charset1 = Charset.forName("MS949");
         System.out.println("charset1 = " + charset1);
@@ -37,5 +37,4 @@ public class AvailableCharsetMain {
         Charset defaultCharset = Charset.defaultCharset();
         System.out.println("defaultCharset = " + defaultCharset);
     }
-
 }
