558c6b3497a9b3036f28798a7b9bda2143065915
KimSeoungWoo <suong22@naver.com>
2026-08-26 14:56:52 +0900

    네트워크 자원정리 공부

---
+++ src/io/file/text/ReadTextFileV2.java
@@ -0,0 +1,42 @@
+package io.file.text;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class ReadTextFileV2 {
+
+    public static final String PATH = "temp/hello2.txt";
+
+    public static void main(String[] args) throws IOException {
+
+        String writeString = "abc\n가나다";
+        System.out.println(" == Write String ==");
+        System.out.println(writeString);
+
+        Path path = Path.of(PATH);
+
+        // 파일에 쓰기
+        Files.writeString(path, writeString, UTF_8);
+        // 파일에서 읽기
+        System.out.println("== Read String ==");
+
+        // 한줄씩 자바 메모리에 올려서 읽음
+        Stream<String> lineStream = Files.lines(path, UTF_8);
+        lineStream.forEach(line -> System.out.println(line));
+
+        lineStream.close();
+
+        // 전체 파일을 읽어 자바 메모리에 올린 후 읽음
+//        List<String> lines = Files.readAllLines(path);
+//        for (int i = 0; i < lines.size(); i++) {
+//            System.out.println((i + 1) + ": " + lines.get(i));
+//        }
+
+
+    }
+
+}
